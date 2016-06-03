092d42e17ec4de6c633f3f5b083b16f4988f0887
Jonathan Arnett <jonarnett90@gmail.com>
2016-06-03 11:26:43 -0400

    Removed deprecated deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-talks=(ES2015 intro-to-javascript redis revim sinatra weblog)
-
-for talk in $talks; do
-  echo "Deploying $talk"
-  rsync -rv $talk/css $talk/js $talk/lib $talk/plugin $talk/index.html jthrrnco@j3rn.com:~/public_html/talks/$talk
-  echo
-done
